2f492bba0625d3703ea3ae280f0c85c126501e9f
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-10-19 23:07:37 +0530

    Using Switch in C#

---
+++ switch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch;
+            Console.WriteLine("enter the 2 values");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                Console.WriteLine("1.addition");
+                Console.WriteLine("2.subs");
+                Console.WriteLine("3.mul");
+                Console.WriteLine("4.div");
+                Console.WriteLine("your choice");
+                ch = Convert.ToInt32(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:Console.WriteLine("sum is " + (a + b));
+                        
+                        break;
+                    case 2: Console.WriteLine("sub is " + (a - b));
+                        break;
+                    case 3: Console.WriteLine("mul is " + (a * b));
+                        break;
+                    case 4: Console.WriteLine("div is " + (a / b));
+                        break;Console.Read();
+                }
+            }
+        }
+    }
+}
