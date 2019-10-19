ec63814992ade13fd5da43c23c991ae03c733cae
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-10-19 23:05:43 +0530

    C# program whether the number is even or odd

---
+++ evenodd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenodd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+         
+            for (int i = 0; i <= 150; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i+"num is even");
+                else
+                    Console.WriteLine(i+"num is odd");
+            }
+            Console.Read();
+        }
+    }
+}
