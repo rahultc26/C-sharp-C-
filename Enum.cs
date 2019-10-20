171112256b805a59822720459f35853cf72241ce
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-10-20 22:56:59 +0530

    Using Enum keyword in C#

---
+++ Enum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    class Program
+    {
+        enum Days
+        {
+            Monday,tuesday,wednesday,thursday,friday,saturday,sunday
+        }
+        static void doWork()
+        {
+
+            Days first = Days.Monday;
+            Console.WriteLine(first);
+            first++;
+            Console.WriteLine(first);
+          
+
+        }
+      
+        static void Main(string[] args)
+        {
+            doWork();
+            Console.Read();
+        }
+   
+}
+    }
+
