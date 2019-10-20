8c00dc4cd60ded86e2c8af6568af8daa140a0fff
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-10-20 22:54:47 +0530

    Typecasting in C#

---
+++ typecast.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace typecast
+{
+    class Example
+    { 
+        class Program { }
+
+    static void Main(string[] args)
+    {
+        Example e = new Example();
+        Program P = (Program)e;
+        Console.Read();
+    }
+    }
+}
