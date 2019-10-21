db4396b17eb2d3c5e7bfd7c9f846732fe284f600
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-10-21 22:40:15 +0530

    To find sum of two numbers

---
+++ sum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace display
+{
+    class Program
+    {
+       void sum()
+        {
+            Console.WriteLine("enter two nums");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = x + y;
+            Console.WriteLine("the sum is " + z);
+            Console.Read();
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.sum();
+        
+        }
+    }
+}
