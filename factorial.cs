65854ca186816faa2c04f1e135dad8e71cf9ff24
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-10-21 22:35:17 +0530

    Program for Factorial

---
+++ factorial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int i, fact = 1, number;
+                Console.WriteLine("Enter any Number: ");
+                number = int.Parse(Console.ReadLine());
+                for (i = 1; i <= number; i++)
+                {
+                    fact = fact * i;
+                }
+                Console.WriteLine("Factorial of " + number + " is: " + fact);
+            }
+            Console.Read();
+        }
+    }
+}
