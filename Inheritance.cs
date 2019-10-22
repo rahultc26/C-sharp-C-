50b981d9923eeca74f65867e4feb84461d14a865
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-10-23 00:12:28 +0530

    inheritance in C#

---
+++ Inheritance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public class Program
+    {
+        public float salary = 50000;
+    }
+    public class Programmer : Program
+    {
+        public float bonus = 10000;
+    }
+    public class Testinheritance
+    {
+        static void Main(string[] args)
+        {
+            Programmer p1 = new Programmer();
+            Console.WriteLine("Salary: "+ p1.salary);
+            Console.WriteLine("Bounus: "+ p1.bonus);
+            Console.Read();
+
+        }
+    }
+}
+
