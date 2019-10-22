946788d9d26603927525a1a1a7d128af217e3c61
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-10-23 00:10:16 +0530

    C# Program to find number is Armstrong or Not

---
+++ armstrong.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace armstrong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, r, sum = 0, temp;
+            Console.WriteLine("Enter any number");
+            n = int.Parse(Console.ReadLine());
+            temp = n;
+            while(n>0)
+            {
+                r = n % 10;
+                sum = sum + (r * r * r);
+                n = n / 10;
+            }
+
+            if (temp == sum)
+                Console.WriteLine("Is Armstrong");
+            else
+                Console.WriteLine("Not Armstrong");
+            Console.Read();
+
+
+        }
+    }
+}
