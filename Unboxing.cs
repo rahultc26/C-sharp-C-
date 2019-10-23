483166ed114056fd986d3f1e0fc3f90e3b5b6cf2
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-10-23 11:49:08 +0530

    Unboxing in C#

---
+++ Unboxing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* */
+
+           // Unboxing
+            int num = 23;
+            object obj = num;  //boxing
+
+            num = (int)obj;  //unboxing
+
+            //display result
+            Console.WriteLine("Value of ob object is :" + obj);
+            Console.WriteLine("value of num is :" + num); 
+            Console.Read();
+        }
+    }
+}
