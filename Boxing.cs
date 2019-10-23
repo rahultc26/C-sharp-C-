6609bcc67698ea6d774ff937a88a10a6e1de2c7a
rahultc26 <54186658+rahultc26@users.noreply.github.com>
2019-10-23 11:48:13 +0530

    Boxing in C#

---
+++ Boxing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2020;
+
+            object obj = num;  //boxing
+
+            num = 100;   //value of num to be change
+            System.Console.WriteLine("value_Type value 0f num is:{0}", num);
+            System.Console.WriteLine("Object-type value of obj is:{0}", obj);
+            Console.Read();
+        }
+    }
+}
